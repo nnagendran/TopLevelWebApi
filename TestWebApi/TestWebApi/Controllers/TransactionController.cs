fc6682dabfda8b689da2617148be7eb7370c9231
Administrator <nnagendranmca07@gmail.com>
2019-03-18 17:44:49 +0530

    test

---
+++ TestWebApi/TestWebApi/Controllers/TransactionController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TestWebApi.Models;
+namespace TestWebApi.Controllers
+{
+    public class TransactionController : ApiController
+    {
+        // GET: api/Transaction
+        [HttpGet]
+        public IEnumerable<Transaction> GetAllTransaction()
+        {
+            Transaction t = new Transaction();
+            Transaction t1 = new Transaction();
+            List<Transaction> li = new List<Transaction>();
+
+            t.ApplicationId = 101;
+            t.Type = "Debit";
+            t.Summary = "Payment";
+            t.Amount = 50;
+            t.PostingDate = DateTime.Now;
+            t.IsCleared = true;
+            t.ClearedDate = DateTime.Now;
+
+            t1.ApplicationId = 102;
+            t1.Type = "Debit";
+            t1.Summary = "Payment";
+            t1.Amount = 100;
+            t1.PostingDate = DateTime.Now;
+            t1.IsCleared = true;
+            t1.ClearedDate = DateTime.Now;
+
+            li.Add(t);
+            li.Add(t1);
+            return li;
+        }
+
+        public IEnumerable<Transaction> Get(int id)
+        {
+            Transaction t = new Transaction();
+            Transaction t1 = new Transaction();
+            List<Transaction> li = new List<Transaction>();
+            if (id == 1)
+            {
+                t.ApplicationId = 101;
+                t.Type = "Debit";
+                t.Summary = "Payment";
+                t.Amount = 50;
+                t.PostingDate = DateTime.Now;
+                t.IsCleared = true;
+                t.ClearedDate = DateTime.Now;
+                li.Add(t);
+            }
+            else
+            {
+                t1.ApplicationId = 102;
+                t1.Type = "Debit";
+                t1.Summary = "Payment";
+                t1.Amount = 100;
+                t1.PostingDate = DateTime.Now;
+                t1.IsCleared = true;
+                t1.ClearedDate = DateTime.Now;
+                li.Add(t1);
+            }
+            return li;
+        }
+
+
+        // POST: api/Transaction
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Transaction/5
+        [HttpPut]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Transaction/5
+        [HttpDelete]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TestWebApi/TestWebApi/Models/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestWebApi.Models
+{
+    public class Transaction
+    {
+        public int ApplicationId { get; set; }
+        public string Type { get; set; }
+        public string Summary { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime PostingDate { get; set; }
+        public bool IsCleared { get; set; }
+        public DateTime ClearedDate { get; set; }
+    }
+}
